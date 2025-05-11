05a44d193437a078da72c66a13dc9cb0e0500f17
Pedro Gonçalves <22503161@aluno.cotemig.com.br>
2025-05-10 21:33:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const images = ["./img/img1.jpg", "./img/img2.jpg","./img/img3.jpg","./img/img4.jpg","./img/img5.jpg", "./img/img6.jpg" , "./img/img7.jpg" , "./img/img8.jpg" , "./img/img9.jpg" , "./img/img10.jpg"]; 
+  let current = 0;
+  const imgElement = document.getElementById("slideshow");
+
+  setInterval(() => {
+    current = (current + 1) % images.length;
+    imgElement.src = images[current];
+  }, 5000); 
