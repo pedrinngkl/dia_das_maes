1590c88d5160baf781a7902fce3e3cf0adc396c8
Pedro Gonçalves <22503161@aluno.cotemig.com.br>
2025-05-10 21:45:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,33 @@ const images = ["./img/img1.jpg", "./img/img2.jpg","./img/img3.jpg","./img/img4.
     current = (current + 1) % images.length;
     imgElement.src = images[current];
   }, 3000); 
+
+// Eventos de toque para celular
+sliderContainer.addEventListener("touchstart", (e) => {
+  dragging = true;
+  updateSliderTouch(e.touches[0]);
+});
+
+window.addEventListener("touchmove", (e) => {
+  if (dragging) {
+    updateSliderTouch(e.touches[0]);
+  }
+});
+
+window.addEventListener("touchend", () => {
+  dragging = false;
+});
+
+function updateSliderTouch(touch) {
+  const rect = sliderContainer.getBoundingClientRect();
+  let width = touch.clientX - rect.left;
+
+  if (width < 0) width = 0;
+  if (width > rect.width) width = rect.width;
+
+  slider.style.width = `${width}px`;
+
+  const percentage = Math.round((width / rect.width) * 100);
+  sliderText.textContent = `Avaliação: ${percentage}% de amor!`;
+}
+
