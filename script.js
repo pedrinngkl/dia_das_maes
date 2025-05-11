f7a56fd26f0404f952460631b39a73f19e6b96c4
Pedro Gonçalves <22503161@aluno.cotemig.com.br>
2025-05-10 22:05:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ const images = ["./img/img1.jpg", "./img/img2.jpg","./img/img3.jpg","./img/img4.
   setInterval(() => {
     current = (current + 1) % images.length;
     imgElement.src = images[current];
-  }, 3000); 
+  }, 4500); 
 
 
